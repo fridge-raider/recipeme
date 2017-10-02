2ab198fd705e621f8029105917efc8825e428146
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-10-02 18:49:27 -0400

    updated models to include nutrients

--- server/db/models/orderHistory.js
+++ server/db/models/orderHistory.js
@@ -1,19 +1,21 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const Ingredient = require('./ingredients')
 
 // how much of each ingredient each user has
 const OrderHistory = db.define('orderHistory', {
-  quantity: {
-    type: Sequelize.INTEGER
-  },
-  units: {
-    type: Sequelize.STRING
+  servings: {
+    type: Sequelize.FLOAT
   },
   price: {
     type: Sequelize.FLOAT
   },
   orderId: {
     type: Sequelize.INTEGER
+  }
+}, {
+  defaultScope: {
+    include: [{model: Ingredient}]
   }
 })
 
