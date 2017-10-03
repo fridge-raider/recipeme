2cb72767b471544cc85a2106a91cd99ac7ff69c4
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-10-03 09:39:42 -0400

    some frontend

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,13 +6,13 @@ import { Container, Menu, Grid } from 'semantic-ui-react'
 
 
 export default class Navbar extends Component {
-  
+
   render() {
     return (
       <Menu color='grey' size='large' inverted secondary className='nav-bar'>
         <Menu.Item>
           <img src='http://cdn.appstorm.net/android.appstorm.net/android/files/2013/08/Logo-Burpple1.png' />
-          <NavLink to='/'>FeedMe</NavLink>
+          <NavLink to='/'>RecipeMe</NavLink>
         </Menu.Item>
         <Menu.Item>
           <NavLink to='/findrecipes'>Find Recipes</NavLink>
