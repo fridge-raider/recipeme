ebfb218a1c4d901e27ecadf3b62a168f5154edf9
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-10-04 01:10:57 -0400

    deleted test statement

--- client/components/UserHome.jsx
+++ client/components/UserHome.jsx
@@ -55,7 +55,6 @@ const mapDispatch = (dispatch) => {
     initialData() {
       dispatch(fetchCategoryOrderHistory())
       dispatch(fetchNutrientOrderHistory())
-      dispatch(getRecipesByDefCategory('Vegetables'))
     }
   }
 }
