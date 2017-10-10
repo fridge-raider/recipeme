64cbca2d46bc6f160f585e58b537d8b0ef1beadf
puhtreeseeya <puhtreeseeya@gmail.com>
2017-10-10 10:42:22 -0400

    homepage and sidebar hardcode

--- bin/seed.js
+++ bin/seed.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize')
-const db = require('./server/db');
-const {Ingredient, NutrientsAPIID, OrderHistory} = require('./server/db/models')
+const db = require('../server/db');
+const {Ingredient, NutrientsAPIID, OrderHistory} = require('../server/db/models')
 
 //var fileName = require('./trainCategories.json');
 // var ingredients = new Set();
 
-let fileNutrientsIng = require('./nutrientsAll.json');
+let fileNutrientsIng = require('./ingredientCategories.js');
 let fileNutID = require('./nutritionID.json')
 
 // //setting up orderhistory promises 
-// const order_histories = require('./order_history_seed.js'); 
-// const all_order_histories = order_histories.map(order_history => { OrderHistory.create(order_history)}); 
+const order_histories = require('./order_history_seed.js'); 
+const all_order_histories = order_histories.map(order_history => { OrderHistory.create(order_history)}); 
 
-const temp = new Set(); 
-const uniqueIngredients = new Set();
+//const temp = new Set(); 
+//const uniqueIngredients = new Set();
 const rowsNut = new Set();
 
 fileNutID.forEach(nutrient => {
@@ -27,41 +27,41 @@ fileNutID.forEach(nutrient => {
 	rowsNut.add(instance)
 })
 
-fileNutrientsIng.forEach(ingredient => {
+// fileNutrientsIng.forEach(ingredient => {
 
-	const instance = { 
-		name: '', 
-		servingQty: 0, 
-		nf_calories: 0.0,
-		nf_total_fat: 0.0,
-		nf_saturated_fat: 0.0,
-  	nf_sodium: 0.0,
-  	nf_total_carbohydrate: 0.0,
-  	nf_dietary_fiber: 0.0,
-  	nf_sugars: 0.0,
-  	nf_protein: 0.0,
-  	nf_potassium: 0.0,
-  	nf_p: 0.0,
-	}
-	instance.name = ingredient.food_name
-	instance.servingQty = ingredient.serving_qty
-	instance.nf_calories = ingredient.nf_calories
-	instance.nf_total_fat = ingredient.nf_total_fat
-	instance.nf_saturated_fat = ingredient.nf_saturated_fat
-	instance.nf_sodium = ingredient.nf_sodium
-	instance.nf_total_carbohydrate = ingredient.nf_total_carbohydrate
-	instance.nf_dietary_fiber = ingredient.nf_dietary_fiber
-	instance.nf_sugars = ingredient.nf_sugars
-	instance.nf_protein = ingredient.nf_protein
-	instance.nf_potassium = ingredient.nf_potassium
-	instance.nf_p = ingredient.nf_p
+// 	const instance = { 
+// 		name: '', 
+// 		servingQty: 0, 
+// 		nf_calories: 0.0,
+// 		nf_total_fat: 0.0,
+// 		nf_saturated_fat: 0.0,
+//   	nf_sodium: 0.0,
+//   	nf_total_carbohydrate: 0.0,
+//   	nf_dietary_fiber: 0.0,
+//   	nf_sugars: 0.0,
+//   	nf_protein: 0.0,
+//   	nf_potassium: 0.0,
+//   	nf_p: 0.0,
+// 	}
+// 	instance.name = ingredient.food_name
+// 	instance.servingQty = ingredient.serving_qty
+// 	instance.nf_calories = ingredient.nf_calories
+// 	instance.nf_total_fat = ingredient.nf_total_fat
+// 	instance.nf_saturated_fat = ingredient.nf_saturated_fat
+// 	instance.nf_sodium = ingredient.nf_sodium
+// 	instance.nf_total_carbohydrate = ingredient.nf_total_carbohydrate
+// 	instance.nf_dietary_fiber = ingredient.nf_dietary_fiber
+// 	instance.nf_sugars = ingredient.nf_sugars
+// 	instance.nf_protein = ingredient.nf_protein
+// 	instance.nf_potassium = ingredient.nf_potassium
+// 	instance.nf_p = ingredient.nf_p
 
-	if(!temp.has(instance.name)) {
-		temp.add(instance.name); 
-		uniqueIngredients.add(instance); 
-	}
+// 	if(!temp.has(instance.name)) {
+// 		temp.add(instance.name); 
+// 		uniqueIngredients.add(instance); 
+// 	}
 
-})
+// })
 
 
 const seed = () => {
@@ -69,7 +69,7 @@ const seed = () => {
 	const nutIdPromises = []; 
 
 
-	uniqueIngredients.forEach(row => {
+	fileNutrientsIng.forEach(row => {
 		ingredientPromises.push(Ingredient.create(row));
 	})
 	
@@ -82,7 +82,7 @@ const seed = () => {
 
 	return Promise.all(totalArrPromise)
 					.then(() => {
-						return OrderHistory.bulkCreate(all_order_histories)
+						//return OrderHistory.bulkCreate(all_order_histories)
 					})
 
 
