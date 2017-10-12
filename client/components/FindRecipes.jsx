3dbb2b491d5186b97bbe931faba7a9259e0fd6e5
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-10-12 15:45:50 -0400

    adding loading screens

--- client/components/FindRecipes.jsx
+++ client/components/FindRecipes.jsx
@@ -8,54 +8,102 @@ import { getRecipesByIngredient } from '../store'
 import { GridList, GridTile } from 'material-ui/GridList';
 import Subheader from 'material-ui/Subheader';
 import SearchBar from 'material-ui-search-bar'
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+import {getRecipes} from '../store'
+import RaisedButton from 'material-ui/RaisedButton'
+
 
 class FindRecipes extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      mainIngredient: ''
+      mainIngredient: '',
+      isLoading: true,
+      hasRequested: false
     }
 
     this.renderSearch = this.renderSearch.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("inside props", nextProps.autopopRecipes);
+    console.log('this.props', this.props)
+    if (this.props !== nextProps) {
+      //checking product has been set from state to props -- error with .length off of null
+        this.setState({isLoading: false})
+    }
+  }
+
+  componentDidMount() {
+    console.log('in component did mount')
+    if (this.props.autopopRecipes.length && !this.state.hasRequested) {
+      this.setState({isLoading: false})
+    }
   }
 
   renderSearch() {
     return (
+      <Grid columns={2}>
+      <Grid.Row>
+      <Grid.Column>
       <SearchBar
         style={{borderRadius:25, maxWidth:"flex"}}
         onChange={(value) => {
           this.setState({mainIngredient: value})
           }}
         onRequestSearch={(evt) => {
-          this.props.handleSubmit(evt, this.state.mainIngredient)
+          this.handleSubmit(evt, this.state.mainIngredient)
         }}
         hintText="Begin new search by ingredient!"
         />
+        </Grid.Column>
+        <Grid.Column>
+        <RaisedButton backgroundColor="#a4c639" style={{borderRadius:"25"}} onClick={(evt)=>this.handleSubmit(evt, this.state.mainIngredient)}>Find Recipes!</RaisedButton>
+        </Grid.Column>
+        </Grid.Row>
+        </Grid>
     )
+  }
+
+  handleSubmit(evt, mainIngredient) {
+    this.setState({isLoading: true, hasRequested: true})
+    this.props.handleSubmit(evt, mainIngredient)
   }
 
   render() {
     //const { getRecipes, autopopRecipes, deficientCategories } = this.props
     let counter = 0;
+    let subtitle = ''
+    if (this.props.categoryHistory.length) {
+      subtitle = `Auto-populated with recipes with ${this.props.deficientCategories.defCategory.toLowerCase()} (your largest deficiency), based on past purchases`
+    }
+    else {
+      subtitle = `Auto-populated with recipes with ${this.props.deficientCategories.defCategory.toLowerCase()} (your largest deficiency)`
+    }
     return (
       <Container fluid style={{ padding: '1em 2em' }}>
         <h2>Recipes</h2>
         {
-          !this.state.mainIngredient.length &&
-          <p style={{fontStyle: 'italic'}}>Auto-populated with recipes with {this.props.deficientCategories.defCategory.toLowerCase()} (your largest deficiency), based on past purchases</p>
+          !this.state.hasRequested &&
+          <p style={{fontStyle: 'italic'}}>{subtitle}</p>
         }
 
         { this.renderSearch() }
         <br />
         <div style={styles.root}>
+        {
+          (this.state.isLoading)
+          ? (<Dimmer active><Loader indeterminate inline>Loading Recipes, Be Back Shortly!</Loader></Dimmer>)
+          : (
 
           <GridList
           cols={3}
           cellHeight={300}
           style={styles.gridList}
           >
-            { !this.state.mainIngredient.length ?
+            { !this.state.hasRequested ?
               this.props.autopopRecipes.map(recipe => {
                 return <RecipeCard key={recipe.id} recipe={recipe} />
               })
@@ -65,7 +113,10 @@ class FindRecipes extends Component {
             }
 
           </GridList>
+          )
+        }
         </div>
+
       </Container>
     )
   }
@@ -88,14 +139,19 @@ const mapProps = (state) => {
   return {
     getRecipes: state.getRecipes,
     autopopRecipes: state.autopopRecipes,
-    deficientCategories: state.deficientCategories
+    deficientCategories: state.deficientCategories,
+    categoryHistory: state.categoryHistory,
   }
 }
 
 const mapDispatch = (dispatch, ownProps) => {
   return {
     handleSubmit: (evt, ingred) => {
+      dispatch(getRecipes([]))
       dispatch(getRecipesByIngredient(ingred))
+    },
+    clearRecipes: () => {
+      dispatch(getRecipes([]))
     }
   }
 }
