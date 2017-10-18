81d3cb0507425e9b661f79860830ee697f0e7e9e
puhtreeseeya <puhtreeseeya@gmail.com>
2017-10-18 19:22:42 -0400

    pushing sockets

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 8080
 const app = express()
 const fs = require('fs');
 const cors = require('cors')
+const socketio = require('socket.io')
+
 
 module.exports = app
 
@@ -85,6 +87,8 @@ const createApp = () => {
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
   const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+  const io = socketio(server)
+  require('./socket')(io)
 }
 
 const syncDb = () => db.sync({force: false})
