f4e4c535435f5dcbf7aba7452663cade25939c5c
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-10-18 17:19:20 -0400

    cleaning

--- client/components/ReceiptUpload.jsx
+++ client/components/ReceiptUpload.jsx
@@ -1,61 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import { NavLink } from 'react-router-dom'
-import { Container, Form, Grid } from 'semantic-ui-react'
-import { addReceipt } from '../store'
-import CheckReceiptIng from './CheckReceiptIng.jsx'
-import ReactS3Uploader from 'react-s3-uploader'
+
 import FineUploaderS3 from 'fine-uploader-wrappers/s3'
 import Gallery from 'react-fine-uploader'
 import RaisedButton from 'material-ui/RaisedButton'
 import 'react-fine-uploader/gallery/gallery.css'
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
-import Dialog from 'material-ui/Dialog';
-import ErrorModal from './ErrorModal.jsx';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import { Dimmer, Loader, Container } from 'semantic-ui-react'
+
 import DailyRecommendedModal from './DailyRecommendedModal.jsx'
-import { setReceipt } from '../store'
+import ErrorModal from './ErrorModal.jsx';
+import { addReceipt, setReceipt } from '../store'
+import CheckReceiptIng from './CheckReceiptIng.jsx'
 
 const bucket_name = 'groceryreceipts'
 const client_access_key = 'AKIAICX6Y4Q4M5VMOXSA'
 
 const uploader = new FineUploaderS3({
   options: {
-      request: {
-          endpoint: `${bucket_name}.s3.amazonaws.com`,
-          accessKey: client_access_key,
-      },
-      template: 'qq-template-s3',
-      cors: {
-        expected: true
+    request: {
+      endpoint: `${bucket_name}.s3.amazonaws.com`,
+      accessKey: client_access_key,
+    },
+    template: 'qq-template-s3',
+    cors: {
+      expected: true
     },
     chunking: {
-        enabled: true
+      enabled: true
     },
     resume: {
-        enabled: true
+      enabled: true
     },
-      signature: {
-          endpoint: "/api/s3/signatureHandler"
-      },
-      uploadSuccess: {
-        endpoint: "/api/s3/uploadSuccessful"
+    signature: {
+      endpoint: '/api/s3/signatureHandler'
+    },
+    uploadSuccess: {
+      endpoint: '/api/s3/uploadSuccessful'
     },
     iframeSupport: {
-        localBlankPagePath: "success.html"
+      localBlankPagePath: 'success.html'
     },
-      thumbnails: {
-        placeholders: {
-            notAvailablePath: "../../public/not_available-generic.png",
-            waitingPath: "../../public/waiting-generic.png"
-        }
+    thumbnails: {
+      placeholders: {
+        notAvailablePath: '../../public/not_available-generic.png',
+        waitingPath: '../../public/waiting-generic.png'
+      }
     }
   }
 })
 
-export class ReceiptUpload extends React.Component {
+export class ReceiptUpload extends Component {
   constructor(props) {
     super(props);
 
@@ -75,115 +71,90 @@ export class ReceiptUpload extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log("inside receipt upload", nextProps.currentReceipt);
-    if(this.props !== nextProps) {
+    if (this.props !== nextProps) {
       //checking product has been set from state to props -- error with .length off of null
-      if(Array.isArray(nextProps.currentReceipt)) {
-        this.setState({isLoading: false})
+      if (Array.isArray(nextProps.currentReceipt)) {
+        this.setState({ isLoading: false })
       }
-      if(nextProps.currentReceipt.length === 0) {
-        this.setState({openError: true})
+      if (nextProps.currentReceipt.length === 0) {
+        this.setState({ openError: true })
       }
     }
   }
 
-  componentDidMount() {
-    uploader.on('complete', (id, name, res) => {
-      if (newStatus === 'submitted') {
-        const submittedFiles = this.state.submittedFiles
-
-        submittedFiles.push(id)
-        this.setState({ submittedFiles })
-      }
-      else if (this.isFileGone(newStatus)) {
-        const submittedFiles = this.state.submittedFiles
-        const indexToRemove = submittedFiles.indexOf(id)
-
-        submittedFiles.splice(indexToRemove, 1)
-        this.setState({ submittedFiles })
-      }
-    })
-
-  }
-
   handleCloseModal() {
-    this.setState({open: false});
+    this.setState({ open: false });
   }
 
   handleSubmit(evt) {
     evt.preventDefault()
     this.props.getReceipt()
-    this.setState({isLoading: true})
+    this.setState({ isLoading: true })
   }
 
   handleOpenQuestion() {
-    this.setState({openQuestion: true})
+    this.setState({ openQuestion: true })
   }
 
   handleManualReceipt() {
-    console.log("hello");
-    const manualItem = { ingredientName: "" , userId: this.props.user.id, servings: 1, price: 0.00, week: Date.now(), createdAt: Date.now() }
+    const manualItem = { ingredientName: '', userId: this.props.user.id, servings: 1, price: 0.00, week: Date.now(), createdAt: Date.now() }
     const manualReceipt = [];
-    for(var i=0; i<5; i++) {
+    for (let i = 0; i < 5; i++) {
       manualReceipt.push(manualItem);
     }
-    this.setState({ isReceipt: false})
+    this.setState({ isReceipt: false })
     this.props.setUserReceipt(manualReceipt);
   }
 
   isFileGone() {
     return [
-        'canceled',
-        'deleted',
+      'canceled',
+      'deleted',
     ].indexOf(status) >= 0
   }
 
   render() {
-  const fileInputChildren = <span>Choose file</span>
-  console.log('is there a current receipt', !!Object.keys(this.props.currentReceipt).length, this.props.currentReceipt.length)
-  return (
-    <div>
-      {
-        (this.state.isLoading)
-        ? (<Dimmer active><Loader indeterminate>Parsing Files, Be Back Shortly</Loader></Dimmer>)
-        : (
-          <div className="ui grid">
-            <div className="one wide column"></div>
-                    <div className="fourteen wide column">
-                      <div style={{width: 400, marginLeft: "auto", marginRight:"auto"}}>
-                        <Container>
-                        { (this.props.currentReceipt.length > 0)
-                          ? <CheckReceiptIng receipt={this.props.currentReceipt} isReceipt={this.state.isReceipt}/>
-                          : <ErrorModal open={this.state.openError} title="Parsing Error" message="Your receipt could not be parsed. We are sorry from the bottom of our hearts. ♥ "/>
-                        }
+    const fileInputChildren = <span>Choose file</span>
+    return (
+      <div>
+        {
+          (this.state.isLoading)
+            ? (<Dimmer active><Loader indeterminate>Parsing Files, Be Back Shortly</Loader></Dimmer>)
+            : (
+              <div className="ui grid">
+                <div className="one wide column" />
+                <div className="fourteen wide column">
+                  <div style={{ width: 400, marginLeft: "auto", marginRight: "auto" }}>
+                    <Container>
+                      {(this.props.currentReceipt.length > 0)
+                        ? <CheckReceiptIng receipt={this.props.currentReceipt} isReceipt={this.state.isReceipt} />
+                        : <ErrorModal open={this.state.openError} title="Parsing Error" message="Your receipt could not be parsed. We are sorry from the bottom of our hearts. ♥ " />
+                      }
 
-                          <Gallery fileInput-children={ fileInputChildren } uploader={ uploader } style={{width: 400}}/>
-                          <RaisedButton
-                          primary={true}
-                          label="Parse Purchases"
-                          fullWidth={true}
-                          onClick={this.handleSubmit}
-                          ></RaisedButton>
-                        </Container>
-                      </div>
-                    </div>
-            <div className="one wide column">
-              <FloatingActionButton mini={true} onClick={this.handleManualReceipt}>
-                <ContentAdd />
+                      <Gallery fileInput-children={fileInputChildren} uploader={uploader} style={{ width: 400 }} />
+                      <RaisedButton
+                        primary={true}
+                        label="Parse Purchases"
+                        fullWidth={true}
+                        onClick={this.handleSubmit}
+                      ></RaisedButton>
+                    </Container>
+                  </div>
+                </div>
+                <div className="one wide column">
+                  <FloatingActionButton mini={true} onClick={this.handleManualReceipt}>
+                    <ContentAdd />
+                  </FloatingActionButton>
+                  <FloatingActionButton style={{ marginTop: 15 }} mini={true} onClick={this.handleOpenQuestion}>
+                    ?
               </FloatingActionButton>
-              <FloatingActionButton style={{marginTop: 15}} mini={true} onClick={this.handleOpenQuestion}>
-                ?
-              </FloatingActionButton>
-            </div>
+                </div>
 
-            <DailyRecommendedModal open={this.state.openQuestion} />
-
-
-
-        </div>
-        )
-      }
-    </div>
+                <DailyRecommendedModal open={this.state.openQuestion} />
+              </div>
+            )
+        }
+      </div>
     )
   }
 }
