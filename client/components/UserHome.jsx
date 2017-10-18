5f94a92e87803f191d01676691ef2804ce462513
Alice Chen <alicejychen15@gmail.com>
2017-10-18 16:33:54 -0400

    cleaned userhome

--- client/components/UserHome.jsx
+++ client/components/UserHome.jsx
@@ -1,26 +1,11 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import { fetchFavoriteRecipes,
-  fetchCategoryOrderHistory,
-  fetchNutrientOrderHistory,
-  fetchDeficientCategories,
-  fetchDeficientNutrients,
-  getRecipesByDefCategory,
-  getRecipesByIngredient,
-  fetchIngredientNames,
-  fetchShoppingList,
-  getRecipeDetails, getRecipes } from '../store'
+import { fetchFavoriteRecipes, fetchCategoryOrderHistory, fetchNutrientOrderHistory, fetchShoppingList, getRecipeDetails, getRecipes } from '../store'
 import SearchBar from 'material-ui-search-bar'
-import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
-import Avatar from 'material-ui/Avatar';
-import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 import Divider from 'material-ui/Divider';
-import {Grid, Container, Menu} from 'semantic-ui-react'
 import Paper from 'material-ui/Paper';
 import GraphVisualizations from './GraphVisualizations.jsx'
-import Favorite from 'material-ui/svg-icons/action/favorite'
 import ShoppingList from './ShoppingList.jsx'
 import FavoritesList from './FavoritesList.jsx'
 
@@ -47,7 +32,7 @@ export class UserHome extends Component {
     return (
 
       <SearchBar
-        style={{borderRadius:25, maxWidth:"90%", marginLeft:20, maxHeight:40, marginBottom:15}}
+        style={{borderRadius: 25, maxWidth: '90%', marginLeft: 20, maxHeight: 40, marginBottom: 15}}
         onChange={(value) => this.setState({search: value})}
         onRequestSearch={(value) => this.setState({submit: value})}
         hintText="Search Favorites"
@@ -61,18 +46,18 @@ export class UserHome extends Component {
     return (
       <div fluid style={{backgroundColor:'#F5F5F5', marginTop:-20}}>
         <div className="ui grid">
-        <div className="row" style={{margin:0}}>
+        <div className="row" style={{margin: 0}}>
           <div className="four wide column">
-            <Paper style={{height:"100%", width:"100%", marginLeft:10, overflowY:"scroll"}} zDepth={2}>
+            <Paper style={{height: '100%', width: '100%', marginLeft: 10, overflowY: 'scroll'}} zDepth={2}>
             <Subheader style={{fontSize: 18}}>Favorite Recipes</Subheader>
               {this.renderSearch()}
-              <FavoritesList search={this.state.search}/>
+              <FavoritesList search={this.state.search} />
               <Divider />
             <Subheader style={{fontSize: 18}}>Shopping List</Subheader>
-             <ShoppingList style={{padding: "0px, 0px"}}/>
+             <ShoppingList style={{padding: '0px, 0px'}} />
             </Paper>
           </div>
-          <div className="twelve wide column" style={{paddingRight:30}}>
+          <div className="twelve wide column" style={{paddingRight: 30}}>
           { (!this.props.categoryHistory)
             ? null
             : (!this.props.categoryHistory.length)? (<h2> You have no past purchasing history. Go Upload a Reciept! </h2>) : null
@@ -113,7 +98,6 @@ const mapDispatch = (dispatch) => {
       dispatch(fetchCategoryOrderHistory())
       dispatch(fetchNutrientOrderHistory())
       dispatch(fetchShoppingList())
-      // dispatch(fetchIngredientNames()) // can do this before logging in to speed up
       dispatch(fetchFavoriteRecipes())
       dispatch(getRecipes([]))
     },
@@ -125,9 +109,3 @@ const mapDispatch = (dispatch) => {
 
 export default connect(mapState, mapDispatch)(UserHome)
 
-/**
- * PROP TYPES
- */
-UserHome.propTypes = {
-  email: PropTypes.string
-}
